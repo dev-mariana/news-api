0f23813342ad4d766723bb0f6c247cc707c9de5e
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-07-21 15:42:34 -0300

    chore: install prisma, prisma client, dotenv and zod

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
 import Fastify from "fastify";
 
 const fastify = Fastify({
@@ -8,11 +10,19 @@ fastify.get("/api", (request, reply) => {
   reply.send({ hello: "world" });
 });
 
-fastify.listen({ port: 3000 }, (err, address) => {
+const port = process.env.PORT || 8080;
+
+const prisma = new PrismaClient();
+
+fastify.listen({ port: Number.parseInt(port.toString()) }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
+
+  prisma.$connect().then(() => {
+    fastify.log.info("Prisma connected");
+  });
 
   fastify.log.info(`Server listening on ${address}`);
 });
