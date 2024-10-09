18a441c7cde4906e3c54bd407a72e8cd5424271c
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2024-10-09 16:42:26 -0300

    chore: add paths config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,6 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
-      enabled: true,
-      include: ["src/**/*.{test, spec}.{ts, js}"],
-      exclude: ["node_modules"],
-    },
-  },
+  plugins: [tsconfigPaths()],
 });
